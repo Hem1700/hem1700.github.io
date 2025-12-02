7be736dab4162b75000332802d064d734f94468b
Hem1700 <hemparekh1596@gmail.com>
2025-12-01 22:11:54 -0800

    Deploy React build

--- app/src/components/BlogList.jsx
+++ app/src/components/BlogList.jsx
@@ -4,28 +4,30 @@ export default function BlogList({ posts, page = 1, totalPages = 1, onPageChange
   return (
     <section className="blog-list-section">
       <div className="container">
-        {posts.map((post) => (
-          <article className="blog-row" key={post.title}>
-            <div className="blog-row-meta">
-              <div className="blog-row-date">{post.date}</div>
-              <div className="blog-row-time">{post.readTime}</div>
-            </div>
-            <div className="blog-row-body">
-              <Link to={post.href} className="blog-row-title">
-                {post.title}
-              </Link>
-              <p className="blog-row-excerpt">{post.excerpt}</p>
-              <div className="blog-tags">
-                <span className="pill">Security</span>
+        <div className="blog-grid">
+          {posts.map((post) => (
+            <article className="blog-row" key={post.title}>
+              <div className="blog-row-meta">
+                <div className="blog-row-date">{post.date}</div>
+                <div className="blog-row-time">{post.readTime}</div>
               </div>
-            </div>
-            <div className="blog-row-cta">
-              <Link to={post.href} className="view-credentials">
-                Read
-              </Link>
-            </div>
-          </article>
-        ))}
+              <div className="blog-row-body">
+                <Link to={post.href} className="blog-row-title">
+                  {post.title}
+                </Link>
+                <p className="blog-row-excerpt">{post.excerpt}</p>
+                <div className="blog-tags">
+                  <span className="pill">Security</span>
+                </div>
+              </div>
+              <div className="blog-row-cta">
+                <Link to={post.href} className="view-credentials">
+                  Read
+                </Link>
+              </div>
+            </article>
+          ))}
+        </div>
 
         {totalPages > 1 && (
           <div className="blog-pagination">
